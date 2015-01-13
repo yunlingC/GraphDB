5e03bdd9034ff9b41104d2c100e0ade340a05cd5
Hiren <hiren.patel@uwaterloo.ca>
2015-01-13 11:25:06 -0500

    minor edits

--- boost-examples/placeg/BundledProperties.cpp
+++ boost-examples/placeg/BundledProperties.cpp
@@ -68,61 +68,62 @@ int main(int,char*[])
 
   // Create a graph object
   Graph* g = new(place) Graph();
+  Graph & gr = *g;
 
   cout << "Address for graph: " << g << endl;
   cout << "Address for memory: " << place << endl;
 
   // Add vertices
-  Graph::vertex_descriptor v0 = boost::add_vertex(*g);
-  Graph::vertex_descriptor v1 = boost::add_vertex(*g);
-  Graph::vertex_descriptor v2 = boost::add_vertex(*g);
-  Graph::vertex_descriptor v3 = boost::add_vertex(*g);
-  Graph::vertex_descriptor v4 = boost::add_vertex(*g);    
-  //  Graph::vertex_descriptor v5 = boost::add_vertex(*g);    
-  //  Graph::vertex_descriptor v6 = boost::add_vertex(*g);    
+  Graph::vertex_descriptor v0 = boost::add_vertex(gr);
+  Graph::vertex_descriptor v1 = boost::add_vertex(gr);
+  Graph::vertex_descriptor v2 = boost::add_vertex(gr);
+  Graph::vertex_descriptor v3 = boost::add_vertex(gr);
+  Graph::vertex_descriptor v4 = boost::add_vertex(gr);    
+  //  Graph::vertex_descriptor v5 = boost::add_vertex(gr);    
+  //  Graph::vertex_descriptor v6 = boost::add_vertex(gr);    
          
   // Add an edge
-  std::pair<Graph::edge_descriptor, bool> e01 = boost::add_edge(v0, v1, *g);
-  std::pair<Graph::edge_descriptor, bool> e02 = boost::add_edge(v0, v2, *g);
-  std::pair<Graph::edge_descriptor, bool> e03 = boost::add_edge(v1, v3, *g);
+  std::pair<Graph::edge_descriptor, bool> e01 = boost::add_edge(v0, v1, gr);
+  std::pair<Graph::edge_descriptor, bool> e02 = boost::add_edge(v0, v2, gr);
+  std::pair<Graph::edge_descriptor, bool> e03 = boost::add_edge(v1, v3, gr);
   
-  std::pair<Graph::edge_descriptor, bool> e04 = boost::add_edge(v0, v4, *g);    
+  std::pair<Graph::edge_descriptor, bool> e04 = boost::add_edge(v0, v4, gr);    
   
   // Set and output the properties of a single vertex
-  (*g)[v0].VertexIntProperty = 0;  (*g)[v0].VertexStringProperty = "zero";
-  (*g)[v1].VertexIntProperty = 1;  (*g)[v1].VertexStringProperty = "one";
-  (*g)[v2].VertexIntProperty = 2;  (*g)[v2].VertexStringProperty = "two";
-  (*g)[v3].VertexIntProperty = 3;  (*g)[v3].VertexStringProperty = "three";
-  (*g)[v4].VertexIntProperty = 4;  (*g)[v4].VertexStringProperty = "four";      
+  (gr)[v0].VertexIntProperty = 0;  (gr)[v0].VertexStringProperty = "zero";
+  (gr)[v1].VertexIntProperty = 1;  (gr)[v1].VertexStringProperty = "one";
+  (gr)[v2].VertexIntProperty = 2;  (gr)[v2].VertexStringProperty = "two";
+  (gr)[v3].VertexIntProperty = 3;  (gr)[v3].VertexStringProperty = "three";
+  (gr)[v4].VertexIntProperty = 4;  (gr)[v4].VertexStringProperty = "four";      
   
   // Set and output the properties of a single edge
-  (*g)[e01.first].EdgeIntProperty = 01;  (*g)[e01.first].EdgeStringProperty = "0-1";
-  (*g)[e02.first].EdgeIntProperty = 02;  (*g)[e02.first].EdgeStringProperty = "0-2";
-  (*g)[e03.first].EdgeIntProperty = 13;  (*g)[e03.first].EdgeStringProperty = "1-3";
-  (*g)[e04.first].EdgeIntProperty = 04;  (*g)[e04.first].EdgeStringProperty = "0-3";      
+  (gr)[e01.first].EdgeIntProperty = 01;  (gr)[e01.first].EdgeStringProperty = "0-1";
+  (gr)[e02.first].EdgeIntProperty = 02;  (gr)[e02.first].EdgeStringProperty = "0-2";
+  (gr)[e03.first].EdgeIntProperty = 13;  (gr)[e03.first].EdgeStringProperty = "1-3";
+  (gr)[e04.first].EdgeIntProperty = 04;  (gr)[e04.first].EdgeStringProperty = "0-3";      
                        
   // Output the properties of each vertex
-  std::pair<Graph::vertex_iterator, Graph::vertex_iterator> vertexIteratorRange = boost::vertices(*g);
+  std::pair<Graph::vertex_iterator, Graph::vertex_iterator> vertexIteratorRange = boost::vertices(gr);
 
   for(Graph::vertex_iterator vertexIterator = vertexIteratorRange.first; vertexIterator != vertexIteratorRange.second; ++vertexIterator)
     {
       //     g[*vertexIterator].VertexIntProperty = 5;
       //     g[*vertexIterator].VertexStringProperty = "test";
-      std::cout << (*g)[*vertexIterator].VertexIntProperty <<  " : " << (*g)[*vertexIterator].VertexStringProperty <<  std::endl;
+      std::cout << (gr)[*vertexIterator].VertexIntProperty <<  " : " << (gr)[*vertexIterator].VertexStringProperty <<  std::endl;
     }
                            
   // Output the properties of each edge
-  std::pair<Graph::edge_iterator, Graph::edge_iterator> edgeIteratorRange = boost::edges(*g);
+  std::pair<Graph::edge_iterator, Graph::edge_iterator> edgeIteratorRange = boost::edges(gr);
   for(Graph::edge_iterator edgeIterator = edgeIteratorRange.first; edgeIterator != edgeIteratorRange.second; ++edgeIterator)
     {
   //     g[*edgeIterator].EdgeIntProperty = 5;
   //     g[*edgeIterator].EdgeStringProperty = "test";
-      //      std::cout << (*g)[*edgeIterator].EdgeIntProperty <<  " : " << (*g)[*edgeIterator].EdgeStringProperty <<  std::endl;
-      cout << source(*edgeIterator, _g) << endl;
+      //      std::cout << (gr)[*edgeIterator].EdgeIntProperty <<  " : " << (gr)[*edgeIterator].EdgeStringProperty <<  std::endl;
+      cout << source(*edgeIterator, gr) << endl;
     }
 
   custom_bfs_visitor vis;
-  breadth_first_search(*g, vertex(0, *g), visitor(vis));
+  breadth_first_search(gr, vertex(0, gr), visitor(vis));
 
   cout << "\n===============================================" << endl;
   for (int i = 0; i < (sizeof(char)*200); i++) {
@@ -133,7 +134,6 @@ int main(int,char*[])
   // Call destructor
   // Must do this manually as the place is used.
   g->~Graph();
-
   
   return 0;
 }
