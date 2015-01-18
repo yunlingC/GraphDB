e7bdaad01255d69a12ea6ad9dd0d18f49a61f201
Hiren <hiren.patel@uwaterloo.ca>
2015-01-17 22:32:53 -0500

    update example to see if I can extract something out

--- examples/offsetof/memlayout.cpp
+++ examples/offsetof/memlayout.cpp
@@ -16,5 +16,17 @@ int main() {
   cout << "A.y: " << offsetof(struct A, y) << " bytes" << endl;
   cout << "A.z: " << offsetof(struct A, z) << " bytes" << endl;
   
+  A a; 
+  a.x = 10; a.y[0] = 12; a.z = 'W';
+  
+
+  void* ap = &a;
+  int* iap = reinterpret_cast<int*>(ap);
+  int* iarr = reinterpret_cast<int*>(ap);
+  int iarr_v = *(iarr+4);
+  char* cap = reinterpret_cast<char*>(ap);
+
+  cout << "a.x: " << *iap << ", a.y[0]: " << *(iarr+8) << *(iarr+4) << ", a.z:" << *(cap+44) << endl;
+  
   return 0;
 };
