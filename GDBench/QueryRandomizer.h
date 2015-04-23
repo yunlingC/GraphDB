7301a4cb1fc20260d0273ff1d97b6a647bdb1611
Yunling Cui <y49cui@uwaterloo.ca>
2015-04-23 17:55:56 -0400

    input changes for more experiments

--- GDBench/QueryRandomizer.h
+++ GDBench/QueryRandomizer.h
@@ -29,51 +29,59 @@ public:
 public:
   QueryRandomizer (GDReader & reader) : _gdReader(reader) { };
 
-  string & getName() {
-    createName(_name);
+  string & getName(unsigned int i ) {
+    createName(_name, i);
     return _name;
   }
 
-  string & getPid() {
-    createPid(_pid);
+  string & getPid(unsigned int i) {
+    createPid(_pid, i);
     return _pid;
   }
 
-  VertexDescriptor & getAPersonIndex() {
-    createPersonIndex(_person);
+  VertexDescriptor & getAPersonIndex(unsigned int i) {
+    createPersonIndex(_person, i);
     return _person;
   }
   
-  VertexDescriptor & getAWebpageIndex() {
-    createWebpageIndex(_webpage);
+  VertexDescriptor & getAWebpageIndex(unsigned int i) {
+    createWebpageIndex(_webpage, i);
     return _webpage;
   }
 
-private:
-  void createName(string & name) {
-    unsigned int nameCounter = _gdReader.getPeopleCounter();
-    unsigned int nameId = rand() % (nameCounter/3);
-    name = _gdReader.getNameList().at(nameId);
+public:
+//  void createName(string & name) {
+  void createName(string & name, unsigned int i) {
+//    unsigned int nameCounter = _gdReader.getPeopleCounter();
+//    unsigned int nameId = rand() % (nameCounter/3);
+//    name = _gdReader.getNameList().at(nameId);
+    name = _gdReader.getNameList().at(i);
     //std::cout << "Random name is " << name << std::endl;
   }
 
-  void createPid(string & pid) {
-    unsigned int pidCounter = _gdReader.getPeopleCounter();
-    unsigned int pidId = rand() % (pidCounter/3);
-    pid = _gdReader.getPidList().at(pidId);
+//  void createPid(string & pid) {
+  void createPid(string & pid, unsigned int i) {
+//    unsigned int pidCounter = _gdReader.getPeopleCounter();
+//    unsigned int pidId = rand() % (pidCounter/3);
+//    pid = _gdReader.getPidList().at(pidId);
+    pid = _gdReader.getPidList().at(i);
     //std::cout << "Random pid is " << pid << std::endl;
   }
 
-  void createPersonIndex(VertexDescriptor & person) {
-    unsigned int personCounter = _gdReader.getPeopleCounter();
-    person = rand() % (personCounter/3);
+//  void createPersonIndex(VertexDescriptor & person) {
+  void createPersonIndex(VertexDescriptor & person, unsigned int i) {
+//    unsigned int personCounter = _gdReader.getPeopleCounter();
+//    person = rand() % (personCounter/3);
+    person = i;
     //std::cout << "Random person is " << person << std::endl;
   }
 
-  void createWebpageIndex(VertexDescriptor & webpage) {
+//  void createWebpageIndex(VertexDescriptor & webpage) {
+  void createWebpageIndex(VertexDescriptor & webpage, unsigned int i) {
     unsigned int personCounter = _gdReader.getPeopleCounter();
-    unsigned int webCounter = _gdReader.getWebpagesCounter();
-    webpage = rand() % (webCounter/3) + personCounter ;
+//    unsigned int webCounter = _gdReader.getWebpagesCounter();
+//    webpage = rand() % (webCounter/3) + personCounter ;
+    webpage = i + personCounter ;
     //std::cout << "Random webpage is " << webpage << std::endl;
   }
   
