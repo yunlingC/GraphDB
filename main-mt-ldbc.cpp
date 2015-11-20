877bb4ada716542a23087911812b785b81534f52
Yunling Cui <y49cui@uwaterloo.ca>
2015-11-20 11:14:28 -0500

    add another two alloc functions

--- main-mt-ldbc.cpp
+++ main-mt-ldbc.cpp
@@ -8,12 +8,65 @@ int main( int argc, char * argv[]) {
   typedef GraphType Graph;
 
   Graph g;
-  LDBCReader reader(g);
 
+#ifdef _FIXALLOC_
+  g.allocVertexMemory(40000);
+  g.allocEdgeMemory(80000);
+#endif /* _FIXALLOC */
+
+
+  LDBCReader reader(g);
   //reader.readDirectory("../tests/ldbc/social_network_100/New");
   reader.readDirectory("/home/y49cui/research/graph/ldbc-graphdb/graphdb/tests/ldbc/social_network_100/New");
 
 //  g.dump();
+
+  cout << "Finish reading\n";
+
+  unsigned int STEP = 100;
+  if (argc > 1) {
+    if ((std::stoi(argv[1]) > 1) && 
+        (std::stoi(argv[1]) < 5000)) {
+
+        STEP = std::stoi(argv[1]);
+    }
+  }
+
+  srand(time(NULL));
+
+  for (unsigned int i = 0; i < 74990; i+= STEP) {
+    auto EdgeId = rand()%74990;
+
+    while (g.Edges.find(EdgeId) != g.Edges.end()) {
+      EdgeId = rand()%74990;
+    }
+
+    g.Edges.insert(EdgeId);
+
+    auto res = g.removeBranch(EdgeId);
+//    auto res = g.removeBranch(i);
+//    std::cout << "remove edge with id " << res << "\n";
+//    fflush(stdout);
+  }
+
+  
+  for (unsigned int j = 1; j < 36800; j+= STEP) {
+    auto VertexId = rand()%36800 + 1;
+
+    while (g.Vertices.find(VertexId) != g.Vertices.end()) {
+      VertexId = rand()%36800;
+    }
+    g.Vertices.insert(VertexId);
+
+    auto res2 = g.removeVertex(VertexId);
+//    auto res2 = g.removeVertex(j);
+//    std::cout << "remove vertex with id " << res2 << "\n";
+//    fflush(stdout);
+  }
+
+//  std::cout <<"recover all the deletion \n";
+
+  g.recoverAll();
 
   AddressVisitor AddrVisitor;
   breadthFirstSearch(g, 0, AddrVisitor);
