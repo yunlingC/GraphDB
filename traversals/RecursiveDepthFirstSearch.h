6b53834f42f1a2782ed84fcd31ff490a6a3019e4
Yunling Cui <y49cui@uwaterloo.ca>
2016-02-29 15:02:58 -0500

    modify RecursiveDFS

--- traversals/RecursiveDepthFirstSearch.h
+++ traversals/RecursiveDepthFirstSearch.h
@@ -17,80 +17,48 @@
 /// Include the GraphType class.
 #include "GraphType.h"
 #include "Visitor.h"
-//#include "Filter.h"
 
 void recursiveDepthFirstSearch(GraphType & Graph,
 			       GraphType::VertexDescriptor VertexSourceId,
-                               Visitor & GraphVisitor,  /// new
-			       std::map<GraphType::VertexPointer, bool> &
-			       VisitedColor) {
+             Visitor & GraphVisitor, 
+			       std::map<GraphType::VertexPointer, bool> & VisitedColor) {
 
   typedef std::pair<GraphType::VertexPointer, bool> ColorMapPair;
-  typedef std::map<GraphType::VertexPointer, bool> ColorMap;
-  typedef std::vector<GraphType::EdgePointer> EdgeList;
 
   GraphType::VertexPointer CurrentVertex =
     Graph.getVertexPointer(VertexSourceId);
   
   VisitedColor.insert(ColorMapPair(CurrentVertex, true));
 
-  //  std::cout << "==> vid: " << CurrentVertex->getId() << "\n";
-
   bool VertexMatch = GraphVisitor.visitVertex(CurrentVertex);
-  if(VertexMatch == true)
+  if (VertexMatch == true)
     return ;
 
-  EdgeList Edges;
-  Edges = Graph.getOutEdges(CurrentVertex);
-
-  /* 
-  //FIXME: There is some issue here that YC needs to fix.
-  unsigned int direction = GraphVisitor.visitDirection(CurrentVertex);
-  switch (direction) {
-    case 1:
-      Edges = Graph.getOutEdges(CurrentVertex);
-      break;
-    case 2:
-      Edges = Graph.getInEdges(CurrentVertex);
-      break;
-    case 0: {
-      auto InEdges = Graph.getInEdges(CurrentVertex);
-      Edges = Graph.getOutEdges(CurrentVertex);
-      Edges.insert( Edges.end(), InEdges.begin(), InEdges.end() );  
-      break;
-
-            }
-    default:
-      std::cout <<"ERROR: Direction Unknown\n";
-  }
-  */
-
-  // Iterate over all the edges.
-  for ( auto EdgeIterator = Edges.begin();  EdgeIterator != Edges.end();
-       ++EdgeIterator ) {
+  auto NextEdge = CurrentVertex->getNextEdge();
+  while (NextEdge != nullptr) {
     GraphType::VertexPointer TargetVertex =
-      (*EdgeIterator)->getTarget(CurrentVertex);
+      NextEdge->getTarget(CurrentVertex);
 
-    bool EdgeMatch = GraphVisitor.visitEdge(*EdgeIterator);
+    GraphVisitor.visitEdge(NextEdge);
 
     bool RevisitFlag = GraphVisitor.discoverVertex(TargetVertex);
 
-    bool BranchMatch = GraphVisitor.scheduleBranch(CurrentVertex, *EdgeIterator, TargetVertex);
-    bool TypeMatch = GraphVisitor.scheduleEdge(*EdgeIterator);
-
+    GraphVisitor.scheduleBranch(CurrentVertex, NextEdge, TargetVertex);
+    bool TypeMatch = GraphVisitor.scheduleEdge(NextEdge);
 
     // Get color and check if false.
     auto VisitedVertex = VisitedColor.find(TargetVertex);
-    if ( VisitedVertex == VisitedColor.end() || RevisitFlag ) {
-//      std::cout << "Vertex " << TargetVertex->getId() << " comes to recursive\n";
-      if( TypeMatch ) {
-        recursiveDepthFirstSearch(Graph, TargetVertex->getId(), GraphVisitor, VisitedColor);
+    if (VisitedVertex == VisitedColor.end() || RevisitFlag) {
+      if (TypeMatch) {
+        recursiveDepthFirstSearch(Graph, TargetVertex->getId(), 
+                                GraphVisitor, VisitedColor);
+        VisitedColor.insert(ColorMapPair(TargetVertex, true));
       }
     }
     else {
-      VisitedColor.insert(ColorMapPair(TargetVertex, true));
-      bool RevisitMatch = GraphVisitor.revisitVertex(TargetVertex);
+      GraphVisitor.revisitVertex(TargetVertex);
     }
+    NextEdge = NextEdge->getNextEdge(CurrentVertex);
   }
 };
 
